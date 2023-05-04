2af5c3b6285ec969b0da11bd03d45b8c80d76345
unknown <chimhoi918@gmail.com>
2023-05-04 13:02:30 +0700

    update task 3_5

---
+++ lib/level_3/task3_5.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Task3_5 extends StatefulWidget {
+  const Task3_5({super.key});
+
+  @override
+  State<Task3_5> createState() => _Task3_5State();
+}
+
+class _Task3_5State extends State<Task3_5> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Task 3_5'),
+        ),
+        body: Container());
+  }
+}
